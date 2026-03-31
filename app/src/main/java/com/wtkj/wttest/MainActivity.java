05112b9a10501b5b1db4860504f4b13c6c1cb518
高文强 <654475990@qq.com>
2026-03-31 14:53:57 +0800

    提交代码

--- app/src/main/java/com/wtkj/wttest/MainActivity.java
+++ app/src/main/java/com/wtkj/wttest/MainActivity.java
@@ -10,6 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        IjkMediaPlayer.native_setLogLevel(IjkMediaPlayer.IJK_LOG_DEBUG);
     }
 }
